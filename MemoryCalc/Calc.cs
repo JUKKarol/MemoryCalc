45139e9e3c4f3a5442b595bac317604b60fea519
Karol Przybylak <karolprzybylak2@gmail.com>
2022-11-18 19:39:00 +0100

    Add final project version #1

---
+++ MemoryCalc/Calc.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCalc
+{
+    static public class Calc
+    {
+
+        public static void Unit(double size, int unit, ref double newSize, ref int newUnit)
+        {
+            newSize = size;
+
+            if (unit == newUnit)
+            {
+
+            }
+            else
+            {
+                if (unit == 1)
+                {
+                    unit = 2;
+                    newSize = size / 8;
+                }
+                else if (newUnit == 1)
+                {
+                    unit -= 1;
+                    newSize = size * 8;
+                }
+
+                if (unit > newUnit)
+                {
+                    while (unit > newUnit)
+                    {
+                        unit--;
+                        newSize *= 1024;
+                    }
+                }
+                else if (unit < newUnit)
+                {
+                    while (unit < newUnit)
+                    {
+                        unit++;
+                        newSize /= 1024;
+                        Console.WriteLine("podzielone");
+                    }
+                }
+            }
+        }
+
+        public static string ReadUnit(int unit)
+        {
+            string stringUnit = "";
+
+            switch (unit)
+            {
+                case 1:
+                    stringUnit = "bitów";
+                    break;
+                case 2:
+                    stringUnit = "bajtów";
+                    break;
+                case 3:
+                    stringUnit = "kilobajtów";
+                    break;
+                case 4:
+                    stringUnit = "megabajtów";
+                    break;
+                case 5:
+                    stringUnit = "gigabajtów";
+                    break;
+                case 6:
+                    stringUnit = "terabajtów";
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+            return stringUnit;
+        }
+
+    }
+}
---
+++ MemoryCalc/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int unit = 0;
+        int newUnit = 0;
+
+        double size = 0;
+        double newSize = 0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxEnter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void buttonCount_Click(object sender, RoutedEventArgs e)
+        {          
+            size = Double.Parse(textBoxEnter.Text);
+            Calc.Unit(size, unit, ref newSize, ref newUnit);
+            labelResultL.Content = Math.Round(size, 2) + " " + Calc.ReadUnit(unit);
+            labelResultR.Content = Math.Round(newSize, 2) + " " + Calc.ReadUnit(newUnit);
+        }
+
+        //ACTUAL BUTTONS
+
+        private void buttonBitUnit_Click(object sender, RoutedEventArgs e)
+        {
+            unit = 1;
+            labelActive1.Content = "b";
+        }
+
+        private void buttonByteUnit_Click(object sender, RoutedEventArgs e)
+        {
+            unit = 2;
+            labelActive1.Content = "B";
+        }
+
+        private void buttonKiloByteUnit_Click(object sender, RoutedEventArgs e)
+        {
+            unit = 3;
+            labelActive1.Content = "KB";
+        }
+
+        private void buttonMegaByteUnit_Click(object sender, RoutedEventArgs e)
+        {
+            unit = 4;
+            labelActive1.Content = "MB";
+        }
+
+        private void buttonGigaByteUnit_Click(object sender, RoutedEventArgs e)
+        {
+            unit = 5;
+            labelActive1.Content = "GB";
+        }
+
+        private void buttonTerraByteUnit_Click(object sender, RoutedEventArgs e)
+        {
+            unit = 6;
+            labelActive1.Content = "TB";
+        }
+
+        //FINAL BUTTONS
+
+        private void buttonBitFUnit_Click(object sender, RoutedEventArgs e)
+        {
+            newUnit = 1;
+            labelActive2.Content = "b";
+        }
+
+        private void buttonByteFUnit_Click(object sender, RoutedEventArgs e)
+        {
+            newUnit = 2;
+            labelActive2.Content = "B";
+        }
+
+        private void buttonKiloByteFUnit_Click(object sender, RoutedEventArgs e)
+        {
+            newUnit = 3;
+            labelActive2.Content = "KB";
+        }
+
+        private void buttonMegaByteFUnit_Click(object sender, RoutedEventArgs e)
+        {
+            newUnit = 4;
+            labelActive2.Content = "MB";
+        }
+
+        private void buttonGigaByteFUnit_Click(object sender, RoutedEventArgs e)
+        {
+            newUnit = 5;
+            labelActive2.Content = "KB";
+        }
+
+        private void buttonTeraByteFUnit_Click(object sender, RoutedEventArgs e)
+        {
+            newUnit = 6;
+            labelActive2.Content = "TB";
+        }
+
+        
+    }
+}
